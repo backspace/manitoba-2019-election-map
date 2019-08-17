e15d76b682a3adcb9537f618bb462f1e920991f3
Buck Doyle <b@chromatin.ca>
2019-08-16 23:11:43 -0500

    Remove unused property

--- app/components/pattern.js
+++ app/components/pattern.js
@@ -16,10 +16,6 @@ export default class PatternComponent extends Component {
     return this.args.patternId.length * this.lineWidth;
   }
 
-  get height() {
-    return this.lineWidth;
-  }
-
   get lines() {
     return this.args.patternId.split('').map((initial, index) => {
       return {
